196cd6ddade68878d923d4a951d70703d88cc1ea
Arturo Aguilar Santos <210716@utxicotepec.edu.mx>
2024-09-26 10:57:51 -0600

    Implementacion de parametros comparados
    Cambios en archivo de Ex06 y readme

--- DarPad/Ex06_Funciones_Parametros.dart
+++ DarPad/Ex06_Funciones_Parametros.dart
@@ -1,98 +1,129 @@
 void main() {
-
-// 1. Llamado de una funcion sin parametros
+    final double totalCarlist;
+    // 1. Llamado de una función sin parámetros
     greetEveryone();
 
-// 2. Llamado de una funcion que retorna valores
-    // a) Sin almacenar el valor en una variable
-    print("Hoy es el dia numero: ${getDayNumber()} del mes");
-    // b) Almacenaremos el valor de la funcion en una nueva variable
-    final int diaMes;
-    diaMes = getDayNumber();
-    print("En tres dias sera: ${diaMes+3}");
+    // 2. Llamado de la función que retorna valores
+    // a) Sin almacenar el dato en una variable
+    print("El día de hoy es: ${getDayNumber()} del mes.");
 
-// 3. Llamado de una funcion con un solo parametro obligatorio
-    // a. Llamado correcto
+    // b) Almacenando el valor de la función en una nueva variable
+    final int diaMes = getDayNumber();
+    print("En tres días será: ${diaMes + 3}");
+
+    // 3. Llamado de una función con un solo parámetro obligatorio
+    // a) Llamado correcto
     print(greetSomeone("Arturo"));
-    // b. Llamado incorrecto con parametros que no son String
-    print(greetSomeone(-321));
+
+    // b) En caso de que la función esté declarada como <dynamic>
+    print(greetSomeone(4));
+    print(greetSomeone(-312));
+    print(greetSomeone(3.1416));
     print(greetSomeone(true));
-    print(greetSomeone(123.4));
-    print(greetSomeone(1));
-// 3.1 Ahora hacemos el mismo experimento pero con la funcion de parametros tipados
-    //print(greetSomeoneType(-321));
-    //print(greetSomeoneType(true));
-    //print(greetSomeoneType(123.4));
-    //print(greetSomeoneType(1));
-    
-// 3.2 No funcionara por que necesita un parametro el cual esta definido como obligatorio
-    //print(greetSomeone());
-    
-// 4. Llamado de una funcion con parametros opcionales
-     //print(greetHourOfDay("Arturo",9));
-     print(greetHourOfDay("Arturo",20));
-     // a) Solo enviado al obligatorio
-     print(greetHourOfDay("Arturo",null));
+    print("----------------------------------------------------------------");
 
-// 5. Funciones Lambda - Las funciones Lambda, mejor conocidas como funciones anonimas o funciones simplificadas, se ejecutan de manera simple
-// y no frecuente en la ejecucion de un programa o sistema
-  
-var calcularCosto = (double productQuantity, double productPrice, double percentageDisscount) => (productQuantity * productPrice)*((100-percentageDisscount)/100);
-  
-double cantidadProducto = 5;
-double precioProducto = 125.50;
-double descuento = 2.5;
-print("""
-  Costo del producto: ${precioProducto}
-  Cantidad: ${cantidadProducto}
-  Descuento: ${descuento}
-  -------------------------------------
-  Costo del carrito de compras: ${calcularCosto(cantidadProducto,
-  precioProducto,descuento)}
-""");
+    // c) Mismo experimento pero ahora con la función de parámetros tipados
+    // print(greetSomeoneTyped("Arturo"));
+    // print(greetSomeoneTyped(4));
+    // print(greetSomeoneTyped(-312));
+    // print(greetSomeoneTyped(3.1416));
+    // print(greetSomeoneTyped(true));
+
+    // 3.2
+    // print(greetSomeone()); // No se puede ejecutar porque si no se definen los parámetros de una función, por default están definidos como obligatorios
+
+    // 4. Llamado de una función con parámetros opcionales
+    //4.1 Enviando parametros
+    print(greetHourOfDay("Arturo", 20));
+    //4.2 solo enviando el obligatorio
+    print(greetHourOfDay("Arturo",null));
+
+    //5. FUNCIONES LAMBDA - las funciones lamdda, o mejor conocidas como funciones anonimas o funciones simplificadas
+    //se ejecutan de manera simple y no frecuente en la ejecucion de un programa o sistema
+    var calculaCosto = (double productQuantity, double productprice,
+                        double percentageDiscount) => (productQuantity * productprice)*((100-percentageDiscount)/100);
+    double PrecioProducto = 5;
+    double cantidadProducto = 125.5;
+    double descuento = 2.5;
+    print("""
+    Costo del producto: ${PrecioProducto};
+    Cantidad: ${cantidadProducto};
+    Descuento: ${descuento}
+    --------------------------------------
+    Costo del Crrito de Compras: ${calculaCosto(cantidadProducto,PrecioProducto,descuento)}
+          """);
+
+    // 6.0 Llamado de una funcion con parametros nombrados
+    print(infoCarListStatus(buyerName: "Gabriela"));
+
+
+    // 6.1 Llamado de una funcion con los parametros en desorden posicional
+
+    print(infoCarListStatus(
+    status: "En espera de pago",
+    amountCarList: 2468.12,
+    buyerName: "Alex"));
+
+
+
 
 }
 
-// Funciones y Parametros
 
-// Declaraciones
-// 1. Funcion sin parametros, sin retorno de valor
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+//  FUNCIONES Y PARÁMETROS 
+
+// 1. FUNCIÓN SIN PARÁMETROS
 void greetEveryone() {
-    print("Hola a todos");
+    print("Hellooo, everyone");
 }
 
-// 2. Funcion sin parametros, retorna datos
-getDayNumber() {
+// FUNCIÓN SIN PARÁMETROS QUE RETORNA DATOS
+int getDayNumber() {
     DateTime now = DateTime.now();
-    int day = now.day;
-    return day;
+    return now.day;
 }
 
-// 3. Funcion que recibe un parametro. En Dart si no se especifica como opcional, siempre sera obligatorio
-String greetSomeone(personName) {
-    return("Hola ${personName}");
+// 3. FUNCIÓN QUE RECIBE UN PARÁMETRO, EN DART SI NO SE ESPECIFICA COMO OPCIONAL, SIEMPRE SERA OBLIGATORIO
+String greetSomeone(dynamic personName) {
+    return "Hola, ${personName}";
 }
 
-//  3.1 Tipamos el parametro de entrada
-
-String greetSomeoneType(String personNameType){
-    return("Hola ${personNameType}");
+// 3.1 Tipando el parámetro de entrada
+String greetSomeoneTyped(String personName) {
+    return "Hola, ${personName}";
 }
 
-// 4. Funcion con parametros opcionales
-String greetHourOfDay(String personName, int? hora) { // int? Puede ser opcional - NullSafety
-  hora ??= DateTime.now().hour; // Si es nulo entocnes ...
+// 4. FUNCIÓN CON PARÁMETROS OPCIONALES
+// int? puede ser opcional  -NullSafety
+String greetHourOfDay(String personName, [int? hourDay]) {
+    hourDay ??= DateTime.now().hour; // Si hora es null, se asigna la fecha actual
+    print("Hora:${hourDay}");
+    String saludo;
 
-  String saludo;
-  print("Hora: ${hora}");
-  
-  if (hora >= 6 && hora < 12) {
-    saludo = "Buenos días"; 
-  } else if (hora >= 12 && hora < 20) {
-    saludo = "Buenas tardes";
-  } else {
-    saludo = "Buenas noches";
-  }
+    if (hourDay >= 6 && hourDay < 12) {
+        saludo = "Buenos días";
+    } else if (hourDay >= 12 && hourDay < 18) {
+        saludo = "Buenas tardes";
+    } else {
+        saludo = "Buenas noches";
+    }
 
-  return "$saludo, $personName";
+    return "${saludo},${personName}";
 }
+
+    String infoCarListStatus(
+    {required String buyerName,
+    double amountCarList = 0.0,
+    String status = "Seleccion de productos"}) {
+        return "El carrito de compras de : ${buyerName}, tiene un total de: \$${amountCarList} y actualmente esta en status: ${status}";
+
+
+    }
+
+
+
+
+
